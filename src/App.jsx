80ed9162ddb739251df8be0352d65a0ff3760515
Somesh Pundir <104303129+SomeshPundir@users.noreply.github.com>
2023-12-30 00:43:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import XDictionaryApp from './components/XDisctionaryApp'
+
+function App() {
+
+
+  return (
+    <>
+   <XDictionaryApp/>
+    </>
+  )
+}
+
+export default App
